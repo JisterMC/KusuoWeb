f19f5199d5b499785ba04912554a6fa7d7b8925d
unknown <emmanuelguevara@hotmail.com>
2021-10-02 08:44:40 -0500

    first commit

---
+++ KusuoStoreIg/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KusuoStoreIg.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "¿Qué somos?";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "¡Contáctanos!";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "¡Bienvenido nuevamente!";
+                return View();
+        }
+    }
+}
