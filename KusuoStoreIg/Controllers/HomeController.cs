9118b86c806df871e6b6c6af218edc030907b3e1
unknown <emmanuelguevara@hotmail.com>
2021-10-30 07:38:40 -0500

    Agregué data

--- KusuoStoreIg/Controllers/HomeController.cs
+++ KusuoStoreIg/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KusuoStoreIg.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,32 @@ namespace KusuoStoreIg.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        KusuoEntity db = new KusuoEntity();
+        // GET: Usuario
+        public ActionResult Registro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registro(usuario usuario)
+        {
+            if (db.usuario.Any(x => x.nomusuario == usuario.nomusuario))
+            {
+                ViewBag.Notification = "Esta cuenta ya existe";
+                return View();
+            }
+            else
+            {
+                db.usuario.Add(usuario);
+                db.SaveChanges();
+
+                Session["IdUsSS"] = usuario.IDus.ToString();
+                Session["UsernameSS"] = usuario.nomusuario.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    
+    public ActionResult Index()
         {
             return View();
         }
@@ -26,10 +52,39 @@ namespace KusuoStoreIg.Controllers
 
             return View();
         }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
         public ActionResult Login()
         {
-            ViewBag.Message = "¡Bienvenido nuevamente!";
-                return View();
+            return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(usuario usuario)
+        {
+            var checkLogin = db.usuario.Where(x => x.nomusuario.Equals(usuario.nomusuario) && x.contraseña.Equals(usuario.contraseña)).FirstOrDefault();
+            if (checkLogin != null)
+            {
+                Session["IdUsSS"] = usuario.IDus.ToString();
+                Session["UsernameSS"] = usuario.nomusuario.ToString();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Su usuario o contraseña es incorrecto";
+            }
+            return View();
+        }
+        public ActionResult Modelos()
+        {
+            ViewBag.Message = "¡Contáctanos!";
+
+            return View();
+        }
+
     }
 }
